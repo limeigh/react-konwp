0ce3b9a5ac0eb2d93d7ff7afa36d033cbb4a740e
dhq <863086441@qq.com>
2017-06-04 21:09:30 +0800

    react-knowp

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+/**
+ * 项目的入口文件
+ */
+
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+import Animate from '../example/Animate.js'
+
+// import Routers from './Routers.js'
+
+ReactDOM.render(
+    <div>
+        <Animate/>
+    </div>,
+    document.getElementById('app')
+)
+
+
+
+
+
+
+
+
+
+
